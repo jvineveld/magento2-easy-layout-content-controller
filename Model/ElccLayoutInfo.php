518f72ea585750ed480163f511920f119de90fec
Jonas van Ineveld <jonas@makeityourown.nl>
2018-02-18 18:53:41 +0100

    fetch template info progres

--- Model/ElccLayoutInfo.php
+++ Model/ElccLayoutInfo.php
@@ -15,9 +15,33 @@ class ElccLayoutInfo  {
 	private function get_template_data($file_path){
 		$raw_xml = file_get_contents($file_path);
 
+		preg_match_all('/^(.*?%%editable:(.*?)%%.*?)$/um', $raw_xml, $editables, PREG_SET_ORDER);
+		preg_match_all('/^\\* layout_info:(.*?):(.*?)$/um', $raw_xml, $layout_info, PREG_SET_ORDER);
+
+		foreach ($editables as &$editable) {
+			preg_match('/name="(.*?)"/um', $editable[1], $name);
+			preg_match('/xsi\:type="(.*?)"/um', $editable[1], $xsi_type);
+
+			$editable = [
+				'line' => trim($editable[1]),
+				'type' => $editable[2],
+				'name' => $name[1],
+				'xsi_type' => $xsi_type[1]
+			];
+		}
+
+		foreach ($layout_info as &$info_line) {
+			$info_line = [
+				'value' => trim($info_line[2]),
+				'type' => $info_line[1]
+			];
+		}
+
 		return [
+			'file_path' => $file_path,
+			'editables' => $editables,
 			'raw' => $raw_xml,
-			'file_path' => $file_path
+			'info' => $layout_info
 		];
 	}
 
